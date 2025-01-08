231fb0336b35c657cb80944fcdc87ebd736b6b40
pragyan <ghimirepragyan100@gmail.com>
2025-01-08 09:17:29 +0545

    food scan screen ui completed

--- Frontend/app/src/main/java/com/app/bitesmart/screens/FoodScanScreen.kt
+++ Frontend/app/src/main/java/com/app/bitesmart/screens/FoodScanScreen.kt
@@ -1,2 +1,76 @@
 package com.app.bitesmart.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.bitesmart.R
+import com.app.bitesmart.widgets.BottomAppBar
+import com.app.bitesmart.widgets.TopAppBar
+
+@Composable
+fun FoodScanScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = { TopAppBar() },
+        bottomBar = { BottomAppBar() }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(space = 32.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.log_in_img_avatar),
+                    contentDescription = "Image that needs to be scanned",
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(2f)
+                )
+                IconButton(
+                    onClick = {
+                        //Todo: capture image
+                    },
+                    modifier = Modifier.size(size = 80.dp)
+                        .clip(shape = CircleShape)
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                ) {
+                    Icon(
+                        Icons.Filled.Search,
+                        contentDescription = "Camera Image",
+                        modifier = Modifier.size(size = 40.dp)
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun FoodScanScreenPreview() {
+    FoodScanScreen()
+}
