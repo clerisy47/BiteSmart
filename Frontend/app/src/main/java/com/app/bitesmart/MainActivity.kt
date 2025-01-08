b07e69edf69fcf03dda1e169157fda4645944188
pragyan <ghimirepragyan100@gmail.com>
2025-01-08 20:52:53 +0545

    asking camera permission

--- Frontend/app/src/main/java/com/app/bitesmart/MainActivity.kt
+++ Frontend/app/src/main/java/com/app/bitesmart/MainActivity.kt
@@ -1,48 +1,64 @@
 package com.app.bitesmart
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.app.bitesmart.screens.FoodScanScreen
 import com.app.bitesmart.ui.theme.BiteSmartTheme
 
 class MainActivity : ComponentActivity() {
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Implement camera related  code
+            } else {
+                // Camera permission denied
+            }
+
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Check if the camera permission is granted
+        when (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+            PackageManager.PERMISSION_GRANTED -> {
+                // Camera permission already granted
+                // Implement camera-related code here
+            }
+            else -> {
+                // Camera permission not granted, request it
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+
         enableEdgeToEdge()
+
         setContent {
             BiteSmartTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    //don't include innerPadding it disturbs the padding of topAppBar and ButtonAppBar
+                    FoodScanScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BiteSmartTheme {
-        Greeting("Android")
+        FoodScanScreen()
     }
 }
