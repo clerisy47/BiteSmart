125fc0485c8e38130654759507923f6e62137819
pragyan <ghimirepragyan100@gmail.com>
2025-01-08 23:51:28 +0545

    sample image checked for text recognition

--- Frontend/app/src/main/java/com/app/bitesmart/MainActivity.kt
+++ Frontend/app/src/main/java/com/app/bitesmart/MainActivity.kt
@@ -2,21 +2,25 @@ package com.app.bitesmart
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
-import com.app.bitesmart.screens.FoodScanScreen
+import com.app.bitesmart.screens.IngredientsScreen
 import com.app.bitesmart.ui.theme.BiteSmartTheme
+import com.app.bitesmart.viewModels.ImageViewModel
 
 class MainActivity : ComponentActivity() {
+
     private val cameraPermissionRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
@@ -45,11 +49,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
     }
     private fun launchApp(){
+        val imageViewModel: ImageViewModel by viewModels()
         setContent {
             BiteSmartTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
                     //don't include innerPadding it disturbs the padding of topAppBar and ButtonAppBar
-                    FoodScanScreen()
+//                    FoodScanScreen(viewModel = imageViewModel)
+                    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ingredients_img_sample)
+                    IngredientsScreen(bitmap = bitmap)
                 }
             }
         }
@@ -62,6 +69,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     BiteSmartTheme {
-        FoodScanScreen()
+//        FoodScanScreen(viewModel = ImageViewModel())
     }
 }
