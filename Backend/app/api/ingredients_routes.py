b7c4d18430778507dcb0050dc05df89f8ba4b9de
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2025-01-10 12:10:16 +0545

    update backend.

--- Backend/app/api/ingredients_routes.py
+++ Backend/app/api/ingredients_routes.py
@@ -1,20 +1,10 @@
 from fastapi import APIRouter, HTTPException
-from typing import List
-from app.schemas.location_schema import LocationRequest, HazardData
-from app.crud.hazard_crud import increment_hazard_count,  get_all_hazards
+from schemas.ingredients_schema import IngredientRequest, IngredientData
+from crud.ingredients_crud import extract_ingridients
 
 router = APIRouter()
 
-@router.post("/report-hazard/", response_model=HazardData)
-async def report_hazard(location: LocationRequest):
-    hazard_data = await increment_hazard_count(location.type, location.latitude, location.longitude)
-    if not hazard_data:
-        raise HTTPException(status_code=404, detail="Error reporting hazard.")
-    return hazard_data
 
-@router.get("/hazards/", response_model=List[HazardData])
-async def get_hazards():
-    hazards = await get_all_hazards()
-    if not hazards:
-        raise HTTPException(status_code=404, detail="No hazard data found.")
-    return hazards
+@router.post("/record-Ingridients/", response_model=IngredientData)
+async def post_text(text: IngredientRequest):
+    return extract_ingridients(text.details)
