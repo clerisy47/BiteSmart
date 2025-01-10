2605e20974c32a9124c3b8db82752910db53256d
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2025-01-10 22:59:33 +0545

    feat: finish backend

--- Backend/app/api/ingredients_routes.py
+++ Backend/app/api/ingredients_routes.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from schemas.ingredients_schema import IngredientRequest, IngredientData
-from crud.ingredients_crud import extract_ingridients
+from schemas.ingredients_schema import IngredientRequest, IngredientItem, IngredientData
+from crud.ingredients_crud import extract_ingridients, get_ingridient
+import json
+
 
 router = APIRouter()
 
 
 @router.post("/record-Ingridients/", response_model=IngredientData)
 async def post_text(text: IngredientRequest):
-    return extract_ingridients(text.details)
+    result_str = extract_ingridients(text.details)
+    print(result_str)
+    result_json = json.loads(result_str)
+    ing_lst = []
+    for info in result_json["information"]:
+        ingredient = await get_ingridient(info)
+        if ingredient:
+            ing_lst.append(ingredient)
+    return IngredientData(items=ing_lst, extra_details=result_json["extra_details"])
