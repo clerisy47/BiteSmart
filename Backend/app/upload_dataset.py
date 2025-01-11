27bc62011970b8d411b27ba2eee7c2ed6f345051
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2025-01-11 11:15:30 +0545

    feat: finsh backend

--- Backend/app/upload_dataset.py
+++ Backend/app/upload_dataset.py
@@ -1,8 +1,8 @@
 import asyncio
 import csv
 import ast
-from crud.ingredients_crud import add_ingridient
-from schemas.ingredients_schema import IngredientItem
+from app.crud.ingredients_crud import add_ingridient
+from app.schemas.ingredients_schema import IngredientItem
 
 
 async def upload_data(csv_file_path: str):
@@ -17,12 +17,12 @@ async def upload_data(csv_file_path: str):
                 name=row["Ingredients"],
                 description=row["Description"],
                 allergies=allergies,
-                source=row["Verified from"],
+                source=row["Verified From"],
             )
             await add_ingridient(ingridient)
             print(f"Added ingredient: {ingridient.name}")
 
 
 if __name__ == "__main__":
-    csv_path = "train_scripts/dataset.csv"
+    csv_path = "train_scripts/dataset_2.csv"
     asyncio.run(upload_data(csv_path))
