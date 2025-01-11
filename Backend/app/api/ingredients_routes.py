ec3a74a5c465568c3df0bf1b28a3dbf93fcbeda6
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2025-01-11 20:51:48 +0545

    feat: fix typo

--- Backend/app/api/ingredients_routes.py
+++ Backend/app/api/ingredients_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.ingredients_schema import IngredientRequest,  IngredientData
-from app.crud.ingredients_crud import extract_ingridients, get_ingridient, add_missing
+from app.crud.ingredients_crud import extract_ingredients, get_ingredient, add_missing
 import json
 
 
@@ -9,11 +9,11 @@ router = APIRouter()
 
 @router.post("/record-ingredients/", response_model=IngredientData)
 async def post_text(text: IngredientRequest):
-    result_str = extract_ingridients(text.details)
+    result_str = extract_ingredients(text.details)
     result_json = json.loads(result_str)
     ing_lst = []
     for info in result_json["information"]:
-        ingredient = await get_ingridient(info)
+        ingredient = await get_ingredient(info)
         if ingredient:
             ing_lst.append(ingredient)
         else:
